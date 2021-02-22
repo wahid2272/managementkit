8a16a654c7d470de333956e8ee8f83c4e34efd71
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-02-22 23:58:48 +0200

    Registration is connected to database

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
-    user: dummy,
-    host: dummy,
-    password:dummy,
-    database:dummy
+  user: "asiaexchange",
+  host: "localhost",
+  password: "pass123",
+  database: "student_portal",
 });
 
+app.post("/register", (req, res) => {
 
-app.listen(3000, ()=> console.log(`Server Running`));
+    const username = req.body.username
+    const password = req.body.password
+
+  db.query(
+    "insert into users (username, password) values (?, ?)",
+    [username, password],
+    (err, result) => {
+      console.log(err);
+    }
+  );
+});
+
+app.listen(3001, () => console.log(`Server Running`));
