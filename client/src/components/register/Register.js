2c554e217a19836c9c372f14d35febd62d6fe7f2
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-02-22 23:55:11 +0200

    Register component connected with backend

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,21 +1,40 @@
-import React, {useState} from 'react';
-import '../../App.css';
+import Axios from "axios";
+import React, { useState } from "react";
+import "../../App.css";
 
 const Register = () => {
+  const [usernameReg, setUsernameReg] = useState("");
+  const [passwordReg, setPasswordReg] = useState("");
 
-    const [usernameReg, setUsernameReg] = useState('');
-    const [passwordReg, setPasswordReg] = useState('');
+  const register = () => {
+    Axios.post("http://localhost:3001/register", {
+      username: usernameReg,
+      password: passwordReg,
+    }).then((response)=> {
+        console.log(response);
+    })
+  };
 
-    return (
-        <div className="registration">
-            <h1>Registration</h1>
-            <label>Username</label>
-            <input type="text" onChange={(e)=>{setUsernameReg(e.target.value)}}/>
-            <label>Password</label>
-            <input type="text" onChange={(e) => {setPasswordReg(e.target.value)}}/>
-            <button>Register</button>
-        </div>
-    );
+  return (
+    <div className="registration">
+      <h1>Registration</h1>
+      <label>Username</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setUsernameReg(e.target.value);
+        }}
+      />
+      <label>Password</label>
+      <input
+        type="text"
+        onChange={(e) => {
+          setPasswordReg(e.target.value);
+        }}
+      />
+      <button onClick={register}>Register</button>
+    </div>
+  );
 };
 
 export default Register;
