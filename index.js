2f728ba2adb29e1984926d88c84ba71030053e11
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-02-26 14:05:39 +0200

    Password hashed and secured for storing in database

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mysql = require("mysql");
 const cors = require('cors');
 const app = express();
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 app.use(express.json());
 app.use(cors());
 
@@ -18,13 +21,20 @@ app.post("/register", (req, res) => {
     const username = req.body.username
     const password = req.body.password
 
-  db.query(
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if(err){
+      console.log(err);
+    }
+    
+    db.query(
     "insert into users (username, password) values (?, ?)",
-    [username, password],
+    [username, hash],
     (err, result) => {
       console.log(err);
     }
   );
+  })
+  
 });
 
 app.post('/login', (req, res) =>{
@@ -32,17 +42,24 @@ app.post('/login', (req, res) =>{
     const password = req.body.password
 
   db.query(
-    "select * from users where username = ? and password = ?",
-    [username, password],
+    "select * from users where username = ?",
+    username,
     (err, result) => {
         if(err){
             res.send({err: err});
         }
 
         if (result.length > 0) {
-            res.send(result)
+            bcrypt.compare(password, result[0].password, (error, response) => {
+              if(response) {
+                res.status(200).send(result);
+              }
+              else {
+                res.send({message: "Wrong username/password combination"})
+              }
+            })
         }else {
-            res.send({message: "Wrong username/password combination"})
+            res.send({message: "User doesn't exist. Please register."})
         }
     }    
   );
