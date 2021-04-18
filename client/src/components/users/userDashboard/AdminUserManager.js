244d4a3f79848ea52fd35ca459df7e3cedf17164
Wahiduzzaman Siddique <wahid2272@gmail.com>
2021-04-18 16:35:29 +0300

    AdminUserManager component created in client side

--- client/src/components/users/userDashboard/AdminUserManager.js
+++ client/src/components/users/userDashboard/AdminUserManager.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import UserNav from '../UserNav';
+import axios from "axios";
 
 const AdminUserManager = () => {
+    const [users, setUsers] =useState([]);
+        useEffect(() => {
+            axios.get(`http://localhost:3005/getAll`)
+                .then(response => setUsers(response.data))              
+        }, []);
+
     return (
         <div>
             <UserNav/>
             <h1>Hello!</h1>
+            <div>{users.map(user=> <li key={user.id}>{user.username}</li>)}</div>
         </div>
     );
 };
